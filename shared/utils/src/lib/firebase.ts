c4d47a0f834fc785da4b6346f2b2ca7a32753d37
Yanick Larouche <yanicklarouche@hotmail.com>
2025-05-12 21:39:07 -0400

    firebase push

---
+++ shared/utils/src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// apps/ease/src/firebase.ts
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'your-app.firebaseapp.com',
+  projectId: 'ease-ng',
+  messagingSenderId: '...',
+  appId: '...',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
